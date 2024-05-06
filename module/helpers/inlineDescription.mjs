0d77237d05dd0fb2e315e3bb5952bc854f6e94b3
Cussa Mitre <cussa.mitre@gmail.com>
2024-05-06 09:31:34 +0100

    ✨ Actor: improve item descriptor

--- module/helpers/inlineDescription.mjs
+++ module/helpers/inlineDescription.mjs
@@ -1,28 +1,38 @@
 import { SWYVERS } from "../config/swyvers.mjs";
 
-export function getInlineDescriptor(item){
+export function getInlineDescriptor(item) {
   switch (item.type) {
     case "weapon":
       return _getInlineDescriptorWeapon(item);
+    case "armour":
+      return _getInlineDescriptorArmour(item);
     default:
       return "";
   }
 }
 
-function _getInlineDescriptorWeapon(weapon){
-    let info = [];
-    info.push(weapon.system.damage);
-    info.push(game.i18n.localize(SWYVERS.WEAPON.LENGTH[weapon.system.length].label));
-    info.push(weapon.system.twoHands ? "2h" : "1h");
-    if (weapon.system.set)
-      info.push(game.i18n.localize("SWYVERS.Weapon.Set"));
-    if (weapon.system.reach)
-      info.push(game.i18n.localize("SWYVERS.Weapon.Reach"));
-    if (weapon.system.halfswording)
-      info.push(game.i18n.localize("SWYVERS.Weapon.Halfswording"));
-    if (weapon.system.armourPiercing)
-      info.push(game.i18n.localize("SWYVERS.Weapon.ArmourPiercing"));
-    if (weapon.system.thrust)
-      info.push(game.i18n.localize("SWYVERS.Weapon.Thrust"));
-    return `${info.join(", ")}`;
+function _getInlineDescriptorWeapon(weapon) {
+  let info = [];
+  info.push(game.i18n.localize(SWYVERS.WEAPON.QUALITY[weapon.system.quality].label));
+  info.push(weapon.system.damage);
+  info.push(game.i18n.localize(SWYVERS.WEAPON.LENGTH[weapon.system.length].label));
+  info.push(weapon.system.twoHands ? "2h" : "1h");
+  if (weapon.system.set)
+    info.push(game.i18n.localize("SWYVERS.Weapon.Set"));
+  if (weapon.system.reach)
+    info.push(game.i18n.localize("SWYVERS.Weapon.Reach"));
+  if (weapon.system.halfswording)
+    info.push(game.i18n.localize("SWYVERS.Weapon.Halfswording"));
+  if (weapon.system.armourPiercing)
+    info.push(game.i18n.localize("SWYVERS.Weapon.ArmourPiercing"));
+  if (weapon.system.thrust)
+    info.push(game.i18n.localize("SWYVERS.Weapon.Thrust"));
+  return `${info.join(", ")}`;
+}
+
+function _getInlineDescriptorArmour(armour) {
+  let info = [];
+  info.push(game.i18n.localize(SWYVERS.ARMOUR.QUALITY[armour.system.quality].label));
+  info.push(armour.system.damageSoak);
+  return `${info.join(", ")}`;
 }
